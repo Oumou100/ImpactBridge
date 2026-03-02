cae295f4120fa2f6a4602dd9777d01fefe37d68a
Oumou100 <dilymalidi10@gmail.com>
2026-03-02 22:23:08 +0000

    feat(contact): export contact module from various component directories

--- apps/web/src/containers/index.ts
+++ apps/web/src/containers/index.ts
@@ -1,6 +1,7 @@
 ﻿export * from "./home";
 export * from "./activities";
 export * from "./services";
+export * from "./contact";
 export * from "./header";
 export * from "./footer";
 export * from "./body";
